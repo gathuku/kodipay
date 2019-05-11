13b82da2f6c0554dac721456b1d70fc9cb5f588d
gathuku <mosesgathuku95@gmail.com>
2019-05-11 12:01:07 +0300

    added payment controller

---
+++ resources/js/components/Payment.vue
@@ -0,0 +1,26 @@
+<template>
+
+</template>
+
+<script>
+
+export default{
+  data(){
+    return{
+      form:{
+
+      }
+    }
+  },
+  methods:{
+    loadPaymet(){
+      
+    }
+  },
+
+  created(){
+    loadPaymet();
+  }
+}
+
+</script>
